75159e88125fba0c13d7c4d56afa9c06642be8d8
Anil <rathodanil.s@gmail.com>
2023-02-28 20:35:36 +0530

    initial commit

---
+++ src/main/java/com/technocrat/BrowserDemo.java
@@ -0,0 +1,37 @@
+package com.technocrat;
+
+import javax.print.attribute.standard.Chromaticity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserDemo {
+
+	public static void main(String[] args) {
+		
+		BrowserDemo browserDemo = new BrowserDemo();
+		browserDemo.launchBrowser("firefox");
+		
+	}
+	
+	public void launchBrowser(String browserType) {
+				
+		WebDriver driver = null;
+		if(browserType.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+			
+		}else if(browserType.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			
+		}else if(browserType.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+			
+		}
+		
+		driver.get("http://google.com");
+		driver.close();
+		
+	}
+}
