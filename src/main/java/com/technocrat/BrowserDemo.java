d7a18f80ae26fb7d20d2b550661c928693410493
Anil <rathodanil.s@gmail.com>
2023-03-01 20:38:44 +0530

    Browser Nagigation

--- src/main/java/com/technocrat/BrowserDemo.java
+++ src/main/java/com/technocrat/BrowserDemo.java
@@ -2,6 +2,7 @@ package com.technocrat;
 
 import javax.print.attribute.standard.Chromaticity;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -9,14 +10,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		BrowserDemo browserDemo = new BrowserDemo();
-		browserDemo.launchBrowser("edge");
+		browserDemo.launchBrowser("chrome");
 		
 	}
 	
-	public void launchBrowser(String browserType) {
+	public void launchBrowser(String browserType) throws Exception {
 				
 		WebDriver driver = null;
 		if(browserType.equalsIgnoreCase("chrome")) {
@@ -30,7 +31,28 @@ public class BrowserDemo {
 			
 		}
 		
+		driver.manage().window().maximize();
+		
 		driver.get("http://google.com");
+		
+		driver.navigate().to("https://github.com/");
+		
+		driver.findElement(By.linkText("Sign in")).click();
+		Thread.sleep(5000);
+		//driver.navigate().back();
+		driver.findElement(By.name("login")).sendKeys("rathodanil.s@gmail.com");
+		Thread.sleep(5000);
+		//driver.navigate().forward();
+		Thread.sleep(5000);
+		
+		driver.findElement(By.id("password")).sendKeys("abcd");
+		driver.findElement(By.name("commit")).click();
+		
+		String message = driver.findElement(By.xpath("//*[@id='js-flash-container']/div/div/div")).getText();
+		System.out.println(message);
+		
+		driver.navigate().refresh();
+		Thread.sleep(5000);
 		driver.close();
 		
 	}
