f7a0ee10a70cf021684e5e29d73fa409bf4a3d59
Anil <rathodanil.s@gmail.com>
2023-03-15 21:18:25 +0530

    Chrome Option

--- src/main/java/com/technocrat/DemoLocators.java
+++ src/main/java/com/technocrat/DemoLocators.java
@@ -6,10 +6,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DemoLocators {
 
 	public static void main(String[] args) throws Exception {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://localhost/login.do");
